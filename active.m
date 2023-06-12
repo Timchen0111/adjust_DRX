606234315f576728578bdec2a70dc3d4760e8814
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2023-06-13 00:28:06 +0800

    fundemental operation

--- active.m
+++ active.m
@@ -1,1 +1,6 @@
-function state = active()
+function active()
+%If ti expired, state = 1. else, = 0
+judge_add = packet(); %A boolean variable
+if packet == true
+    %clear buffer
+end
--- main.m
+++ main.m
@@ -1,15 +1,25 @@
-function result = main() %Parameters
+function result = main(T_ds,T_dl,T_i,T_n) %Parameters
 state = 0;
+wake = 0;
+sleep = 0;
 %0: active 1: light sleep 2: deep sleep
 for t = 1:10000 %may be change!
     switch state
         case 0
-            active()
+            ti = ti-1;
+            wake = wake+1;
+            active();
+            if ti == 0
+                state = 1;
+                tds = T_ds;
+                tn = T_n;
+            end
         case 1
             light_sleep()
         case 2
             deep_sleep()
     end
 end
+PS = sleep/(wake+sleep);
 result = [PS,D]; %PS: power saving vector, D: wake up delay
 
