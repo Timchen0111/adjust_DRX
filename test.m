a8bf7672291960d4445a27011a31740909365b22
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2023-06-12 23:47:40 +0800

    test

---
+++ test.m
@@ -0,0 +1,16 @@
+function result = main() %Parameters
+state = 0;
+%0: active 1: light sleep 2: deep sleep
+for t = 1:10000 %may be change!
+    switch state
+        case 0
+            active()
+        case 1
+            light_sleep()
+        case 2
+            deep_sleep()
+    end
+end
+
+result = [PS,D]; %PS: power saving vector, D: wake up delay
+
