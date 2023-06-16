73e8c6f5e4828aaccab4d791c6b64871897ad543
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2023-06-16 16:59:22 +0800

    Call function "packet generator"

--- main.m
+++ main.m
@@ -6,33 +6,34 @@ total_time = 0;
 buffer = zeros(1,4);  %This matrix record the size and generated time of data stored in the buffer!
 delay = []; %An array record every delay of packet calls
 packet_call_end = true;
-
+ti = T_i;
 %0: active 1: light sleep 2: deep sleep
-for t = 1:10000 %may be change!
+for t = 1:10000 
     %Put data into buffer
     total_time = total_time+1;
-    if packet_generator(1) > 0
-        if packet_generator(3) == 1
+    packet_generated = packet_generator(); 
+    if packet_generated(1) > 0
+        if packet_generated(3) == 1
             packet_call_end = true;
         else
             packet_call_end = false;
         end
         if buffer(1,2) == 0 %buffer is empty
             buffer(1,1) = total_time;
-            buffer(1,2) = packet_generator(1);
-            buffer(1,3) = packet_generator(2);
-            buffer(1,4) = packet_generator(3);
+            buffer(1,2) = packet_generated(1);
+            buffer(1,3) = packet_generated(2);
+            buffer(1,4) = packet_generated(3);
         else
             buffer(end+1,1) = total_time;
-            buffer(end+1,2) = packet_generator(1);
-            buffer(end+1,3) = packet_generator(2);
-            buffer(end+1,4) = packet_generator(3);
+            buffer(end+1,2) = packet_generated(1);
+            buffer(end+1,3) = packet_generated(2);
+            buffer(end+1,4) = packet_generated(3);
         end
     end
     switch state
         case 0
             if buffer(1,2) > 0 %buffer is not empty
-                ti = Ti;
+                ti = T_i;
                 Size = size(buffer);
                 if Size(1) > 1 %buffer includes more than 1 unit of data                    
                     if buffer(1,4) == 1
